b55e32a0f622d0805fab8dbe442d6ee497f42c9a
Paul <paul.wei607@gmail.com>
2022-03-15 02:00:01 -0700

    added static file/prep for heroku deployment

--- movies/connect.py
+++ movies/connect.py
@@ -4,16 +4,18 @@ import pprint
 
 api_key = config('api_key')
 
-movie_id = 550
+movie_id = 500
 
 api_base_url = 'https://api.themoviedb.org/3'
 
-endpoint_path = f"/movie/{movie_id}"
+endpoint_path = f"/movie/top_rated"
 
-endpoint = f"{api_base_url}{endpoint_path}?api_key={api_key}"
+endpoint = f"{api_base_url}{endpoint_path}?api_key={api_key}&language=en-US&page=1"
 
 r = requests.get(endpoint).json()
 
-pprint.pprint(r)
+results = r['results']
 
-pprint.pprint(r["production_companies"][2].get('name'))
+if len(results) > 0:
+    for result in results:
+        print(result['title'])
